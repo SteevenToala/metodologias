a3df5d2c7ca9c0beefedb74b3297e495b885e40e
SteevenToala <165206479+SteevenToala@users.noreply.github.com>
2024-04-03 09:46:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # Redireccionar al archivo PHP
+    return redirect("archivo.php")
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
